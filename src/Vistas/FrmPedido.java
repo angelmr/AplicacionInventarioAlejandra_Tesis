e49d86721f5ed694b4805342b052b23380afc99d
angelmr <angelrc1998@hotmail.com>
2021-02-09 13:58:28 -0500

    Formulario Pedidos

--- src/Vistas/FrmPedido.java
+++ src/Vistas/FrmPedido.java
@@ -9,12 +9,12 @@ package Vistas;
  *
  * @author Angel Ramos
  */
-public class FrmOrden extends javax.swing.JFrame {
+public class FrmPedido extends javax.swing.JFrame {
 
     /**
      * Creates new form Orden
      */
-    public FrmOrden() {
+    public FrmPedido() {
         initComponents();
     }
 
@@ -315,21 +315,23 @@ public class FrmOrden extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmOrden.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmPedido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmOrden.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmPedido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmOrden.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmPedido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmOrden.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmPedido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrmOrden().setVisible(true);
+                new FrmPedido().setVisible(true);
             }
         });
     }
